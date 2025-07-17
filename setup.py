319c70e92e396fb3eeb19b0dd09d6790aebd8cde
waveware4ai <50553398+waveware4ai@users.noreply.github.com>
2025-07-18 00:21:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.2',
     author='14mhz',
     author_email='14mhz@hanmail.net',    
-    description='This small package is used to import at module level instead of package level.',
+    description='This small package is used to import at module/package level.',
     keywords='python import package mudule lazy',
     url='https://github.com/waveware4ai/importpy',
     long_description=open('README.md', 'r', encoding='UTF8').read(),
